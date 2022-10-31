2ce92637f0aeca24f568dd6042fd873c544049af
Subramanya K S <subramanyaks2205@gmail.com>
2022-10-31 07:25:08 +0530

    added model

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/urlShortener', {
+  useNewUrlParser: true, useUnifiedTopology: true
+})
 const app = express();
 app.set('view engine','ejs');
 app.get('/',(req,res)=>{
     res.render('index');
+});
+
+app.post('/shortUrls',(req,res)=>{
+
 })
 
 app.listen(process.env.PORT || 4000);
