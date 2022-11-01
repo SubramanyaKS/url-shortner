fe500d3249d837cd213b996b4a6a23b1be28cc68
Subramanya K S <subramanyaks2205@gmail.com>
2022-11-01 07:18:16 +0530

    updated server

--- server/server.js
+++ server/server.js
@@ -1,18 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const shortUrls = require('./model/shortUrl');
+const ShortUrl = require('./model/shortUrl');
 
 mongoose.connect('mongodb://localhost/urlShortener', {
   useNewUrlParser: true, useUnifiedTopology: true
 })
+
 const app = express();
+
 app.set('view engine','ejs');
-app.get('/',(req,res)=>{
-    res.render('index');
-});
+app.use(express.urlencoded({ extended: false }));
 
-app.post('/shortUrls',(req,res)=>{
 
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrl.find()
+  res.render('index', { shortUrls: shortUrls })
 })
 
+
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl })
+
+  res.redirect('/')
+})
+
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+  if (shortUrl == null) return res.sendStatus(404)
+
+  shortUrl.clicks++
+  shortUrl.save()
+
+  res.redirect(shortUrl.full)
+})
 app.listen(process.env.PORT || 4000);
